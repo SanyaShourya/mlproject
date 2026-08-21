70e3e7c17f4b6ee667ff0dfca8363f70ce5249b5
SanyaShourya <sanayshourya1803@gmail.com>
2026-08-21 11:45:37 +0530

    Hyperparameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -40,7 +40,7 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-        except Exception as p:
+        except Exception as e:
             raise CustomException(e,sys)
 
 if __name__=="__main__":
